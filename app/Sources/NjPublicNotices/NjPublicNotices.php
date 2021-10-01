85bcc7faf25f665e7722945587476bbea0678d68
Chris Hardie <chris@chrishardie.com>
2021-10-01 14:17:09 -0400

    Include publication name and guid, move search IDs to config

--- app/Sources/NjPublicNotices/NjPublicNotices.php
+++ app/Sources/NjPublicNotices/NjPublicNotices.php
@@ -60,7 +60,12 @@ class NjPublicNotices extends BaseSource
         $jar = $client->getCookieJar();
         $session_id = $jar->get('ASP.NET_SessionId')->getValue();
 
-        $crawler = $client->request('GET', $base_url .'/Search.aspx?SSID=8028');
+        $crawler = $client->request(
+            'GET',
+            $base_url .'/Search.aspx?' . Arr::query(
+                ['SSID' => config('bloomfieldfeeds.source_misc.njpublicnotices_actual_search_id')]
+            )
+        );
 
         $response = $client->getInternalResponse();
         if (200 !== $response->getStatusCode()) {
@@ -87,23 +92,44 @@ class NjPublicNotices extends BaseSource
             $notice_id = $link_row->filterXPath('//td//input[contains(@name, "hdnPKValue")]')->attr('value');
             $pub_date = $link_row->filterXPath('//td')->last()->filterXpath('//div[@class="left"]//span')->text();
             $pub_date = preg_replace('/Posted: /', '', $pub_date);
+            $pub_name = $link_row->filterXPath('//div[@class="left"]//strong')->text();
 
             if (! empty($notice_id) && ! empty($session_id)) {
-                $url = $base_url . '/Details.aspx?' . Arr::query(['ID' => $notice_id, 'SID' => $session_id ]);
+                $url = $base_url . '/Details.aspx?' . Arr::query(['ID' => $notice_id ]);
             } else {
                 Log::debug('Cannot find notice ID on NJ Public Notice parsing');
-                $url = $base_url .'/Search.aspx#searchResults';
+                continue;
             }
+
+            // Some day this might work when we can solve captchas :)
+            //            // Visit the details page
+            //            $crawler = $client->request('GET', $url);
+            //
+            //            $response = $client->getInternalResponse();
+            //            if (200 !== $response->getStatusCode()) {
+            //                throw new SourceNotCrawlable('Cannot get notice details', 0, null, $source);
+            //            }
+            //
+            //            $pubName = $crawler->filterXPath('//div[@id="detail"]//span[contains(@id, "lblPubName")]')->text();
+            //            $content = $crawler->filterXPath('//article//span[contains(@id, "lblContentText")]')->text();
 
             // Get the row that has the description/title
             $description_row = $row->filterXPath('//td//table[@class="nested"]//tr')->last();
             $description = $description_row->filter('td')->text('No description available');
+            $description = preg_replace('/ ... click \'view\' to open the full text./', '...', $description);
 
+            // Populate the RSS item. Add notice ID to search query URL for uniqueness. Set GUID to proper details URL.
             $items[] = array(
-                'title' => Str::limit($description, 55),
+                'title' => $pub_name . ': ' . Str::limit($description, 55),
                 'description' => $description,
                 'pubDate' => Carbon::createFromFormat('!m/d/Y', $pub_date, 'America/New_York'),
-                'url' => $url,
+                'url' => $base_url
+                    . '/SmartSearch/SearchResults.aspx?'
+                    . Arr::query([
+                        'SSID' => config('bloomfieldfeeds.source_misc.njpublicnotices_link_search_id'),
+                        'ITEMID' => $notice_id
+                    ]),
+                'guid' => $url,
             );
         }
 

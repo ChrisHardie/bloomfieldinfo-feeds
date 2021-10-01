94787764daee6fe11258ee03e74138eed0b31178
Chris Hardie <chris@chrishardie.com>
2021-10-01 14:11:19 -0400

    Move search feed ids into config

--- config/bloomfieldfeeds.php
+++ config/bloomfieldfeeds.php
@@ -6,4 +6,9 @@ return [
         'njpublicnotices_username' => env('SOURCE_NJPUBLICNOTICES_USERNAME'),
         'njpublicnotices_password' => env('SOURCE_NJPUBLICNOTICES_PASSWORD'),
     ],
+
+    'source_misc' => [
+        'njpublicnotices_actual_search_id' => 8028,
+        'njpublicnotices_link_search_id' => 6742,
+    ],
 ];
